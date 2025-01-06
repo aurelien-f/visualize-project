d833a7cd3b50646a166078b062268076dbaa543f
aurelien-f <feuillard.a@gmail.com>
2025-01-06 11:14:25 +0100

    hide console.log

--- src/components/common/LoadingScreen.tsx
+++ src/components/common/LoadingScreen.tsx
@@ -1,17 +1,10 @@
 "use client";
-import { useEffect } from 'react';
 
 type LoadingScreenProps = {
   progress: number;
 };
 
 export default function LoadingScreen({ progress }: LoadingScreenProps) {
-  useEffect(() => {
-    if (progress === 0) {
-      console.warn('La progression est à 0. Vérifiez que la valeur est bien mise à jour par le parent');
-    }
-    console.log('Progression actuelle:', progress, typeof progress);
-  }, [progress]);
 
   return (
     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col gap-4 items-center justify-center bg-[#241a1a] z-50">
